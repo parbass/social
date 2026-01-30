cefa82fb7382e8b3453bbd97b55ec884b09a1193
cristina-hidalgo-tecnativa <cristina.hidalgo@tecnativa.com>
2026-01-30 12:14:36 +0100

    [MIG] mail_activity_cancel_tracking: Migration to 18.0

--- mail_activity_cancel_tracking/static/tests/tours/mail_activity_cancel_tracking.esm.js
+++ mail_activity_cancel_tracking/static/tests/tours/mail_activity_cancel_tracking.esm.js
@@ -1,19 +1,21 @@
-/* @odoo-module */
-
 import {registry} from "@web/core/registry";
 
 registry.category("web_tour.tours").add("mail_activity_cancel_tracking_done", {
     test: true,
     steps: () => [
         {
-            trigger: ".o-mail-Activity span:contains('Mark Done')",
+            trigger: ".o-mail-Chatter",
         },
         {
-            trigger: ".o-mail-ActivityMarkAsDone button[aria-label='Done']",
+            trigger: "button.o-mail-Activity-markDone",
+            run: "click",
+        },
+        {
+            trigger: "button[aria-label='Done']",
+            run: "click",
         },
         {
             trigger: ".o-mail-Message:contains('done'):contains('Play Mario Kart')",
-            isCheck: true,
         },
     ],
 });
@@ -21,11 +23,14 @@ registry.category("web_tour.tours").add("mail_activity_cancel_tracking_cancel", 
     test: true,
     steps: () => [
         {
-            trigger: ".o-mail-Activity span:contains('Cancel')",
+            trigger: ".o-mail-Chatter",
+        },
+        {
+            trigger: ".o-mail-Activity button.btn-danger:contains('Cancel')",
+            run: "click",
         },
         {
             trigger: ".o-mail-Message:contains('canceled'):contains('Play Mario Kart')",
-            isCheck: true,
         },
     ],
 });
